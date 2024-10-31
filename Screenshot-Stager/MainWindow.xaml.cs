48dbc77b1dc4f00b927f25d9ff570a529bf560d5
Joseph Finney <josephfinney@live.com>
2024-10-30 22:16:48 -0500

    Add project files.

---
+++ Screenshot-Stager/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Screenshot_Stager;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ChangeSizeBTN_Click(object sender, RoutedEventArgs e)
+    {
+        // random number between 1 and 100
+        int diff = new Random().Next(-100, 100);
+        Height += diff;
+        Width += diff;
+    }
+
+    private void ListWindows_Click(object sender, RoutedEventArgs e)
+    {
+        IDictionary<nint, string> windows = OpenWindowGetter.GetOpenWindows();
+
+        foreach (KeyValuePair<nint, string> window in windows)
+        {
+            WindowsText.Text += window.Value + Environment.NewLine;
+        }
+    }
+}
