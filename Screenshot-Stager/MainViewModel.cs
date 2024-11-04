191ebd866a4e9b7fdeaa38d003a4499e81a8f398
Joseph Finney <josephfinney@live.com>
2024-11-04 16:36:18 -0600

    Add take screenshot

--- Screenshot-Stager/MainViewModel.cs
+++ Screenshot-Stager/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Drawing;
+using System.IO;
 using System.Windows;
 
 namespace Screenshot_Stager;
@@ -28,6 +30,8 @@ public partial class MainViewModel : ObservableRecipient
 
     [ObservableProperty]
     private bool isOptionsFlyoutOpen = true;
+
+    private int screenshotIndex = 1;
 
     public MainViewModel()
     {
@@ -70,5 +74,56 @@ public partial class MainViewModel : ObservableRecipient
             };
             WindowList.Add(details);
         }
+    }
+
+    [RelayCommand]
+    private void TakeScreenshot()
+    {
+        if (SelectedWindow is not WindowDetails window)
+            return;
+
+        int titleBarHeight = 42;
+
+        // get dpi of this window
+        double dpi = WindowMethods.GetScaleForHwnd(window.Handle);
+
+        int gapsPadding = 7;
+
+        int screenshotX = (int)((Left + gapsPadding) / dpi);
+        int screenshotY = (int)((Top + titleBarHeight + gapsPadding) / dpi);
+        int screenshotWidth = (int)((Width - (2 * gapsPadding)) / dpi);
+        int screenshotHeight = (int)((Height - ((2 * gapsPadding) + titleBarHeight)) / dpi);
+
+        // take a screenshot of the specified region
+        Bitmap screenshot = GetRegionOfScreenAsBitmap(screenshotX, screenshotY, screenshotWidth, screenshotHeight);
+
+        // save or process the screenshot as needed
+        // e.g., save to file
+        string fileName = $"{window.Title}-{screenshotIndex:D2}.bmp";
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), fileName);
+
+        try
+        {
+            screenshot.Save(path);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error saving screenshot: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        finally
+        {
+            screenshotIndex++;
+        }
+
+    }
+
+    public static Bitmap GetRegionOfScreenAsBitmap(int x, int y, int width, int height)
+    {
+        Bitmap bmp = new(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        using Graphics g = Graphics.FromImage(bmp);
+
+        g.CopyFromScreen(x, y, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+
+        return bmp;
     }
 }
