9e7ef3ba351fa90c067c0c9d03ccc2265ba03102
Joseph Finney <josephfinney@live.com>
2024-11-05 22:55:02 -0600

    Add is null Xaml converter

---
+++ Screenshot-Stager/Converters/IsNullConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Screenshot_Stager.Converters;
+
+public class IsNullConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (parameter is "true")
+            return value is not null;
+
+        return value is null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
